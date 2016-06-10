ff83261e998f515f3df1c3591e2cfac42d730fc5
jdconnors <jconnors@cisco.com>
2016-06-10 16:36:37 -0700

    changes as of the end of the 'JPA Entity Code Assignment' lesson

--- src/main/java/com/example/domain/Customer.java
+++ src/main/java/com/example/domain/Customer.java
@@ -1,10 +1,20 @@
 package com.example.domain;
 
+import javax.persistence.*;
+
 /**
  * Created by jconnors on 6/2/16.
  */
+@Entity
 public class Customer implements DomainObject {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+
+    @Version
+    private Integer version;
+
     private String firstName;
     private String lastName;
     private String email;
@@ -22,6 +32,10 @@ public class Customer implements DomainObject {
     public void setId(Integer id) {
         this.id = id;
     }
+
+    public Integer getVersion() { return version; }
+
+    public void setVersion(Integer version) { this.version = version; }
 
     public String getFirstName() {
         return firstName;
