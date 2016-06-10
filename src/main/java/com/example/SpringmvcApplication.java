01583b8687505e2da6f6b13913bb3ab4137831c3
jdconnors <jconnors@cisco.com>
2016-06-10 15:53:54 -0700

    changes as of the end of the 'Maven Dependencies' lesson

--- src/main/java/com/example/SpringmvcApplication.java
+++ src/main/java/com/example/SpringmvcApplication.java
@@ -2,11 +2,18 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringmvcApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringmvcApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(SpringmvcApplication.class, args);
+
+		System.out.println("Beans ******");
+		System.out.println("bean count = " + ctx.getBeanDefinitionCount());
+		for (String name : ctx.getBeanDefinitionNames()) {
+			System.out.println(name);
+		}
 	}
 }
